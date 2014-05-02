ecc53dd4a70d4d35f8f61ab48b465f4e88ff4fcf
Jan-Home <soulofnoob@ymail.com>
2014-05-02 21:22:11 +0200

    Fixed Upload

--- mobile/index.php
+++ mobile/index.php
@@ -35,63 +35,154 @@
 		}
 	?>
     <script>
-		function getTable(filename) {
-       		var chat = "fehler";
-			console.log("filename: "+filename);
-			$.ajax({
-				url: "processjson.php",
-				data: { file: filename },
-				type: "POST",
-				success: function(response) { chat = jQuery.parseJSON(response); }
-			}).done(function() {	
-				console.log(chat);
-				var table = '<table data-role="table" id="chattable" data-mode="columntoggle" class="ui-body-d ui-shadow table-stripe ui-responsive" data-column-btn-theme="b" data-column-btn-text="Columns to display..." data-column-popup-theme="a">';
-				table = table + '<thead> <tr class="ui-bar-d"> <th data-priority="3">Jahr</th> <th data-priority="2">Datum</th> <th data-priority="1">Zeit</th> <th data-priority="1">Verfasser</th> <th data-priority="1">Nachricht</abbr></th> </tr> </thead>';
-				table = table + '<tbody>';
-				var i = 0;
-				console.log("start generating");
-				for(line in chat){
-					i++;
-					//console.log(chat[line]);
-					//console.log(chat[1].datetime);
-				//##### Split the date ####
-					ds = chat[line].datetime; //29.03.14 22:20:18 
-					var first_part = ds.split(" ")[0],
-					day = first_part.split(".")[0],
-					month = first_part.split(".")[1],
-					year = "20"+first_part.split(".")[2],
-					last_part = ds.split(" ")[1],
-					hour = last_part.split(":")[0],
-					minute =  last_part.split(":")[1],
-					second =  last_part.split(":")[2];
-				//##### Split the date ####
-					//console.log(day+' '+month+' '+year+' '+hour+' '+minute+' '+second);
-					
-					var date = new Date ( Date.UTC ( year, month, day, hour, minute, second ) );
-					//console.log(date);
-					table = table + '<tr class=\"custom_row\"><td>'+year+'</td><td>'+day+'.'+month+'</td><td>'+hour+'.'+minute+'</td><td nowrap>'+chat[line].name+'</td><td>'+chat[line].message+'</td><tr>';
-				}
-				table = table + '</tbody>';
-				console.log("table generated");
-				//console.log(table);
-				$("#chattable").html("");
-				console.log("field empty");
-				$("#chattable").html(table);
-				console.log("table written");
-				//$("#chattable").trigger("create");
-				console.log("table created");
-				$.mobile.navigate( "#view" );
-				console.log("view switched");
-				
+       	var chat = "fehler";
+   		var zeilen = 50;
+   		var currentFile = null;
+   	/*########## File Uplaod ##########*/
+   		var files;
+
+		function prepareUpload(event){files = event.target.files;}
+		function uploadFiles(event){
+		  event.stopPropagation(); // Stop stuff happening
+		    event.preventDefault(); // Totally stop stuff happening
+
+		    // START A LOADING SPINNER HERE
+		 
+		    // Create a formdata object and add the files
+			var data = new FormData();
+			$.each(files, function(key, value){
+				data.append(key, value);
 			});
+		    
+		    $.ajax({
+		        url: 'upload.php?files',
+		        type: 'POST',
+		        data: data,
+		        cache: false,
+		        dataType: 'json',
+		        processData: false, // Don't process the files
+		        contentType: false, // Set content type to false as jQuery will tell the server its a query string request
+		        success: function(data, textStatus, jqXHR){
+		        	if(typeof data.error === 'undefined'){
+		        		// Success so call function to process the form
+		        		//submitForm(event, data);
+		        		console.log("data:");
+		        		console.log(data.files[0]);
+		        		$('#filename').append('<option value="'+data.files[0]+'">'+data.files[0]+'</option>');
+		        		$('#filename').val(data.files[0]);
+		        		$.mobile.navigate( "#overview" );
+		        		
+		        	}else{
+		        		// Handle errors here
+		        		console.log('ERRORSa: ' + data.error);
+		        	}
+		        },
+		        error: function(jqXHR, textStatus, errorThrown){
+		        	// Handle errors here
+		        	console.log('ERRORSb: ' + textStatus);
+		        	// STOP LOADING SPINNER
+		        }
+		    });
+		}
+		function submitForm(event, data){
+		  // Create a jQuery object from the form
+			$form = $(event.target);
+			
+			// Serialize the form data
+			var formData = $form.serialize();
+			
+			// You should sterilise the file names
+			$.each(data.files, function(key, value){
+				formData = formData + '&filenames[]=' + value;
+			});
+		 
+			$.ajax({
+				url: 'submit.php',
+		        type: 'POST',
+		        data: formData,
+		        cache: false,
+		        dataType: 'json',
+		        success: function(data, textStatus, jqXHR){
+		        	if(typeof data.error === 'undefined'){
+		        		// Success so call function to process the form
+		        		console.log('SUCCESS: ' + data.success);
+		        	}else{
+		        		// Handle errors here
+		        		console.log('ERRORS: ' + data.error);
+		        	}
+		        },
+		        error: function(jqXHR, textStatus, errorThrown){
+		        	// Handle errors here
+		        	console.log('ERRORS: ' + textStatus);
+		        },
+		        complete: function(){
+		        	// STOP LOADING SPINNER
+		        }
+			});
+		}
+   	/*########## File Uplaod ##########*/
+		function generateTable(chat) {	
+			console.log(chat);
+			var table = '<table data-role="table" id="chattable" data-mode="columntoggle" class="ui-body-d ui-shadow table-stripe ui-responsive" data-column-btn-theme="b" data-column-btn-text="Columns to display..." data-column-popup-theme="a">';
+			table = table + '<thead> <tr class="ui-bar-d"> <th data-priority="4">Zeile</th> <th data-priority="3">Jahr</th> <th data-priority="1">Datum</th> <th data-priority="2">Zeit</th> <th data-priority="1">Verfasser</th> <th data-priority="1">Nachricht</abbr></th> </tr> </thead>';
+			table = table + '<tbody>';
+			var i = 0;
+			console.log("start generating");
+			for(line in chat){
+				i++;
+			//##### Split the date ####
+				ds = chat[line].datetime; //29.03.14 22:20:18 
+				var first_part = ds.split(" ")[0],
+				day = first_part.split(".")[0],
+				month = first_part.split(".")[1],
+				year = "20"+first_part.split(".")[2],
+				last_part = ds.split(" ")[1],
+				hour = last_part.split(":")[0],
+				minute =  last_part.split(":")[1],
+				second =  last_part.split(":")[2];
+			//##### Split the date ####
+				zeilen = $("#zeilen").val();
+				var date = new Date ( Date.UTC ( year, month, day, hour, minute, second ) );
+				if (i<=zeilen){
+					table = table + '<tr class=\"custom_row\"><td>'+i+'</td><td>'+year+'</td><td>'+day+'.'+month+'</td><td>'+hour+'.'+minute+'</td><td>'+chat[line].name+'</td><td>'+chat[line].message+'</td><tr>';
+				}
+			}
+			table = table + '</tbody>';
+			console.log("table generated");
+			$("#chatwrapper").html(table)
+			console.log("table written");
+			$("#chatwrapper").trigger("create");
+			console.log("table created");
+			
+		}
+
+		function getTable(fileName) {
+			console.log("filename: "+fileName);
+			if(currentFile === null || currentFile !== fileName){
+				console.log("-ajax- currentFile: "+currentFile+", fileName: "+fileName);
+				currentFile = fileName;
+				$.ajax({
+					url: "processjson.php",
+					data: { file: fileName },
+					type: "POST",
+					success: function(response) { chat = jQuery.parseJSON(response); }
+				}).done(function() {
+					generateTable(chat);
+				});
+			}else if(chat !== "fehler"){
+				generateTable(chat);
+			}
         }
+
         $( document ).ready(function() {
+        	$('input[type=file]').on('change', prepareUpload);
+        	$('#button2').on('click', uploadFiles);
         	if(filename !== false){
-        		getTable(filename);
+        		getTable(fileName);
         	}
         	$("#callAjax").click( function(){
-        		var filename = $("#filename").val();
-        		getTable(filename);
+        		var fileName = $("#filename").val();
+        		getTable(fileName);
         	});
         });
     </script>
@@ -120,22 +211,11 @@
 	<div data-role="header">
 		<h1>Hochladen</h1>
 	</div><!-- /header -->
-	<?php
-	if ($_FILES['chatfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['chatfile']['tmp_name'])) { //checks that file is uploaded
-		$path = "txts/".$_FILES['chatfile']['name'];
-		echo $path;
-		if (!file_exists($path)){
-			if (!move_uploaded_file($_FILES['chatfile']['tmp_name'], $path)){
-			    die('Datei wurde nicht Erfolgreich hochgeladen');
-			}
-		}else{
-			echo "Datei vorhanden: <br />";
-		}
-	}
-	?>
+
 	<div role="main" class="ui-content">
 		<p>Hier kannst du deinen von Whatsapp exportierten Chat hochladen.</p>
-		<form action="index.php" method="POST" enctype="multipart/form-data" id="newchat">
+		<form id="newchat">
+			<!--
 			<label for="chatname">Unter welchem Namen soll der Chat gespeichert werden: </label>
 		        <input name="chatname" type="text">
 		    <br />
@@ -148,10 +228,11 @@
 		   	<label for="password">Bestätige das Passwort zum schutz deines Chats: </label>
 		        <input name="password" type="password">
 		    <br />
+			-->
 			<label for="chatfile">Hier die von Whatsapp generierte textdatei hochladen: </label>
 				<input type="file" id="input-chatfile" name="chatfile">
 			<br />
-			<input type="submit" value="Auswerten">
+			<input type="button" id="button2" value="Auswerten">
 		</form>
 		<p><a href="#start" data-direction="reverse" class="ui-btn ui-shadow ui-corner-all ui-btn-b">Zurück zur Startseite</a></p>
 	</div><!-- /content -->
@@ -183,26 +264,17 @@
 			}
 			?>
 		</select></p>
+		<p>
+		    <div class="ui-field-contain">
+		        <div data-role="rangeslider" data-mini="true">
+		            <label for="zeilen">Zeilen:</label>
+		            <input type="range" name="zeilen" id="zeilen" min="50" max="10000" value="50">
+		        </div>
+		    </div>
+		</p>
         <input id="callAjax" type="button" value="Auswerten" />
         <p><a href="#start" data-direction="reverse" class="ui-btn ui-shadow ui-corner-all ui-btn-b">Zurück zur Startseite</a></p>
-	</div><!-- /content -->
-
-	<div data-role="footer">
-		<h4><a href="https://github.com/SoulOfNoob/WhatsappChatBackup">https://github.com/SoulOfNoob/WhatsappChatBackup</a></h4>
-	</div><!-- /footer -->
-</div><!-- /page -->
-
-<!-- Start of third page -->
-<div data-role="page" id="view">
-
-	<div data-role="header">
-		<h1>Auswertung: </h1>
-	</div><!-- /header -->
-
-	<div role="main" class="ui-content">
-		<p><a href="#overview" class="ui-btn ui-shadow ui-corner-all ui-btn-b">Zurück zur Übersicht</a></p>
-		<p><a href="#start" data-direction="reverse" class="ui-btn ui-shadow ui-corner-all ui-btn-b">Zurück zur Startseite</a></p>
-		<div id="chattable"></div>
+        <div id="chatwrapper"></div>
 	</div><!-- /content -->
 
 	<div data-role="footer">
--- mobile/processjson.php
+++ mobile/processjson.php
@@ -1,13 +1,13 @@
 <?php
-date_default_timezone_set("Europe/Berlin");
-$names = array();
-$count = array();
-$beigetreten = array();
-$verlassen = array();
-$entfernt = array();
-$bildaenderung = array();
-$betreffaenderung = array();
 if(isset($_REQUEST["file"])){
+	date_default_timezone_set("Europe/Berlin");
+	$names = array();
+	$count = array();
+	$beigetreten = array();
+	$verlassen = array();
+	$entfernt = array();
+	$bildaenderung = array();
+	$betreffaenderung = array();
 	$handle = @fopen("txts/".$_REQUEST["file"], "r"); //read line one by one
 	$data = array();
 	    $i = 0;
@@ -23,14 +23,24 @@ if(isset($_REQUEST["file"])){
 		    $data[$i]["name"] = $buffer[1];
 			if (strpos($data[$i]["name"], "beigetreten")!==false){
 				$beigetreten[] = $data[$i]["name"];
+				unset($data[$i]["name"]);
+				$i--;
 			}elseif (strpos($data[$i]["name"], "Gruppe verlassen")!==false){
 				$verlassen[] = $data[$i]["name"];
+				unset($data[$i]["name"]);
+				$i--;
 			}elseif (strpos($data[$i]["name"], "‬entfernt")!==false){
 				$entfernt[] = $data[$i]["name"];
+				unset($data[$i]["name"]);
+				$i--;
 			}elseif (strpos($data[$i]["name"], "‬Gruppenbild")!==false){
 				$bildaenderung[] = $data[$i]["name"];
+				unset($data[$i]["name"]);
+				$i--;
 			}elseif (strpos($data[$i]["name"], "Betreff")!==false){
 				$betreffaenderung[] = $data[$i]["name"];
+				unset($data[$i]["name"]);
+				$i--;
 			}else{
 				unset($buffer[1]);
 				$buffer = implode(": ", $buffer);
@@ -46,17 +56,17 @@ if(isset($_REQUEST["file"])){
 	    	$data[$i]["message"].= "<br />".$line;
 	    }
 	}
-
-	//$tablestring = '<table data-role="table" id="table-custom-2" data-mode="columntoggle" class="ui-body-d ui-shadow table-stripe ui-responsive" data-column-btn-theme="b" data-column-btn-text="Columns to display..." data-column-popup-theme="a">';
-	//$tablestring = '<thead> <tr class="ui-bar-d"> <th data-priority="2">Datum</th> <th data-priority="1">Zeit</th> <th data-priority="1">Verfasser</th> <th data-priority="1">Nachricht</abbr></th> </tr> </thead>';
-	//$tablestring.= '<tbody>';
-	//foreach($data as $line){
-	//	$tablestring.= "<tr class=\"custom_row\"><td>".$data[4]["datetime"]->format('d.m')."</td><td>".$data[4]["datetime"]->format('H:i')."</td><td nowrap>".$line["name"]."</td><td>".$line["message"]."</td><tr>";
-	//	
-	//}
-	//$tablestring.= "</tbody>";
-
-	//echo $tablestring;
 	echo json_encode($data);
 }
+/*if ($_FILES['chatfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['chatfile']['tmp_name'])) { //checks that file is uploaded
+	$path = "txts/".$_FILES['chatfile']['name'];
+	echo $path;
+	if (!file_exists($path)){
+		if (!move_uploaded_file($_FILES['chatfile']['tmp_name'], $path)){
+		    die('Datei wurde nicht Erfolgreich hochgeladen');
+		}
+	}else{
+		echo "Datei vorhanden: <br />";
+	}
+}*/
 ?>
